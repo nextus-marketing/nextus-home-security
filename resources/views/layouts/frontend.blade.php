d7e03dd2134f5ebd4e0797bbbc6a7b9a241c2788
Nextus Marketing <marketingteamnextus@gmail.com>
2026-01-15 20:07:58 +0530

    changes done

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Place favicon.ico in the root directory -->
-    <link rel="shortcut icon" type="image/x-icon" href="/frontend/my_img/new-logo/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="/frontend/my_img/favicon.png">
 
     <!-- CSS here -->
     <link rel="stylesheet" href="/frontend/assets/css/bootstrap.css">
